d308fb4afc84919490c25b0042d4f4972bb76862
Dawson Hensel <dawsonhensel@gmail.com>
2022-08-17 07:37:56 -0700

    print output in a grid for easier viewing

--- main.go
+++ main.go
@@ -29,10 +29,27 @@ func main() {
 	// Generate anagrams and extract valid strings of English words.
 	permutation.FindAnagramCandidates(unsortedAnagram, 0, len(unsortedAnagram))
 
+	wordsPerLine := 6
+	wordsPrinted := 0
+	var wordsToPrint []string
+
 	// Print anagrams.
 	if len(hashmap.AnagramCandidates) > 0 {
+		anagramsCount := len(hashmap.AnagramCandidates) - 1
+		i := 0
 		for anagram := range hashmap.AnagramCandidates {
-			fmt.Println(anagram)
+			wordsToPrint = append(wordsToPrint, anagram)
+			wordsPrinted++
+
+			if wordsPrinted%wordsPerLine == 0 {
+				fmt.Println(assembleAnagramsOnSameLine(wordsToPrint))
+				// Empty our words to print slice.
+				wordsToPrint = nil
+				// Print all remaining words if we are iterating over the last entry in `AnagramCandidates`.
+			} else if anagramsCount == i {
+				fmt.Println(assembleAnagramsOnSameLine(wordsToPrint))
+			}
+			i++
 		}
 
 		timeFinish := time.Now()
@@ -43,4 +60,25 @@ func main() {
 		fmt.Println("Not a single anagram was found. oof.")
 	}
 
+}
+
+// Assembles however many words are in the given slice on one string.
+// We can outut more results into a grid in the terminal this way.
+func assembleAnagramsOnSameLine(anagrams []string) string {
+	if len(anagrams) == 0 {
+		return ""
+	}
+
+	var output string
+	wordsOnLine := 0
+	for _, anagram := range anagrams {
+		if wordsOnLine == 0 {
+			output = anagram
+		} else {
+			output += "\t" + anagram
+		}
+		wordsOnLine++
+	}
+
+	return output
 }
